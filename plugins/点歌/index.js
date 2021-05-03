55f6dac71c59c8f10a3a61f4d8460b392cf3f74b
takayama <takayama@foxmail.com>
2021-05-03 11:50:26 +0900

    add an example plugin (点歌)

---
+++ plugins/点歌/index.js
@@ -0,0 +1,42 @@
+"use strict"
+const http = require("http")
+const { segment } = require("oicq")
+
+/**
+ * @this {import("oicq").Client}
+ * @param {import("oicq").MessageEventData} data 
+ */
+function listener(data) {
+    if (data.raw_message.startsWith("点歌")) {
+        const word = data.raw_message.replace("点歌", "").trim()
+        if (!word)
+            return
+        http.get(`http://s.music.163.com/search/get/?type=1&s=${word}&limit=1`, res=>{
+            res.on("data", chunk=>{
+                try {
+                    const id = JSON.parse(String(chunk))?.result?.songs?.[0]?.id
+                    if (id)
+                        data.reply(segment.music("163", id))
+                    else
+                        data.reply("未找到歌曲：" + word)
+                } catch (e) {
+                    this.logger.error("请求歌曲API遇到错误：")
+                    this.logger.error(e)
+                }
+            })
+        }).on("error", (e)=>{
+            this.logger.error(e)
+        })
+    }
+}
+
+function activate(bot) {
+    bot.on("message", listener)
+}
+function deactivate(bot) {
+    bot.off("message", listener)
+}
+
+module.exports = {
+    activate, deactivate,
+}
